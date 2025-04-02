52802aeea4bd4af1e0780bdda48b1e4798b13ce4
lmorgan2398 <lmorgan2398@gmail.com>
2025-04-02 11:44:28 -0500

    Rename image files and add images to dynamic HTML rendering

--- src/home.js
+++ src/home.js
@@ -1,3 +1,6 @@
+import aboutImageFile1 from './images/about-image-1.jpg';
+import aboutImageFile2 from './images/about-image-2.jpg';
+
 export function renderHome(content){
     // Create 'site-home' div, add it to site, and add style class
     const siteHome = document.createElement('div');
@@ -48,7 +51,64 @@ export function renderHome(content){
         const button = document.createElement('button');
         button.textContent = 'MORE ABOUT US';
         aboutSectionText.appendChild(button);
+
+        const aboutSectionImages = document.createElement('div');
+        aboutSection.appendChild(aboutSectionImages);
+        aboutSectionImages.classList.add('about-section-images');
+
+        const aboutImage1 = document.createElement('img');
+        aboutImage1.src = aboutImageFile1;
+        aboutSectionImages.appendChild(aboutImage1);
+
+        const aboutImage2 = document.createElement('img');
+        aboutImage2.src = aboutImageFile2;
+        aboutSectionImages.appendChild(aboutImage2);
     })();
 
 
 }
+
+
+// <div class="site-home">
+// <div class="hero-section">
+//     <h1>CHEZ VOUS</h1>
+//     <p>AN EXQUISITE FRENCH DINING EXPERIENCE IN THE COMFORT 
+//         OF YOUR OWN HOME
+//     </p>
+//     <button>EXPLORE OUR MENU</button>
+// </div>
+// <div class="about-section">
+//     <div class="about-section-text">
+//         <h2>OUR <em>'JE NE SAIS QUOI'</em></h2>
+//         <p>Started as a hobby among friends and family during the COVID-19 pandemic, Chez Nous 
+//             strives to create an atmospheric escape to a French dining hall from the comfort of 
+//             your own home by catering a full-course dinner right out of your home kitchen.
+//         </p>
+//         <button>MORE ABOUT US</button>
+//     </div>
+//     <div class="about-images">
+//         <img src="./images/lee-vue-P-oXFsK6STQ-unsplash.jpg" alt="A tray of cheese and fruit">
+//         <img src="./images/dan-rooney-xn_I632JDjk-unsplash.jpg" alt="A glass of wine on a candlelit table">
+//     </div>
+// </div>
+// <div class="reserve-section">
+//     <div class="hours">
+//         <h2>HOURS OF OPERATION</h2>
+//         <p>All dinner services are provided by reservation only, limited 
+//             to evening hours on the weekend. Available starting times are 
+//             early because of the expansive nature of the full-course dinner. 
+//             Expect the event to run for several hours.
+//         </p>
+//         <p>You may schedule a start time during the following hours:</p>
+//         <p>Fridays 6:00 p.m. - 8:00 p.m.</p>
+//         <p>Saturdays 5:00 p.m. - 9:00 p.m.</p>
+//         <p>Sundays 5:00 p.m. - 8:00 p.m.</p>
+//     </div>
+//     <button class="reserve">MAKE A RESERVATION</button>
+// </div>
+// <div class="contact-section">
+//     <h3>CHEZ VOUS</h3>
+//     <p>Phone: xxx-xxx-FAKE</p>
+//     <p>Email: notarealemail@fakeservice.com</p>
+// </div>
+// </div>
