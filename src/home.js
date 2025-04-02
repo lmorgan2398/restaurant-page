78725c6ba00c6e7859e7f339a5bb1334e4ff6958
lmorgan2398 <lmorgan2398@gmail.com>
2025-04-01 20:33:02 -0500

    Add function to render about section elements

--- src/home.js
+++ src/home.js
@@ -24,5 +24,31 @@ export function renderHome(content){
         heroSection.appendChild(button);
     })();
 
+    (function renderAboutSection() {
+        const aboutSection = document.createElement('div');
+        siteHome.appendChild(aboutSection);
+        aboutSection.classList.add('about-section');
+
+        const aboutSectionText = document.createElement('div');
+        aboutSection.appendChild(aboutSectionText);
+        aboutSectionText.classList.add('about-section-text');
+
+        const h2 = document.createElement('h2');
+        h2.textContent = 'OUR ';
+        aboutSectionText.appendChild(h2);
+
+        const em = document.createElement('em');
+        em.textContent = '\'JE NE SAIS QUOI\'';
+        h2.appendChild(em);
+
+        const p = document.createElement('p');
+        p.textContent = 'Started as a hobby among friends and family during the COVID-19 pandemic, Chez Nous strives to create an atmospheric escape to a French dining hall from the comfort of your own home by catering a full-course dinner right out of your home kitchen.';
+        aboutSectionText.appendChild(p);
+
+        const button = document.createElement('button');
+        button.textContent = 'MORE ABOUT US';
+        aboutSectionText.appendChild(button);
+    })();
+
 
 }
